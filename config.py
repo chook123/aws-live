ffd5903fa39f2d647621216f90a224fb75f329c6
chook123 <167161130+chook123@users.noreply.github.com>
2024-04-16 12:51:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.clugy08g0x2s.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chookjingfoong-employee"
+customregion = "us-east-1"
+
